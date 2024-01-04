7bf5dd0d229dd14bd60cb5276fef3b525cec304e
atik81 <mdatikujaman@81gmail.com>
2024-01-04 05:07:51 +0000

    l

--- yapi.py
+++ yapi.py
@@ -30,17 +30,40 @@ def get_video_id(video_url):
 def clean_text(text):
     clean_text = re.sub(r'<.*?>', '', text)
     return clean_text
-
-# Function to analyze sentiment of a comment
-def analyze_sentiment_vader(comment):
+def analyze_sentiment_vader_all(comment):
+    global positive_count, negative_count, neutral_count
     sentiment = analyzer.polarity_scores(comment)
     compound_score = sentiment['compound']
     if compound_score > 0:
+        positive_count += 1
+
+    elif compound_score < 0:
+        negative_count += 1
+
+    else:
+        neutral_count += 1
+def analyze_comments(comments):
+    sentiment_counts = {'positive': 0, 'negative': 0, 'neutral': 0}
+    for comment in comments:
+        sentiment = analyze_sentiment_vader(comment)
+        sentiment_counts[sentiment] += 1
+    return sentiment_counts
+# Function to analyze sentiment of a comment
+def analyze_sentiment_vader(comment):
+    global positive_count, negative_count, neutral_count
+    sentiment = analyzer.polarity_scores(comment)
+    compound_score = sentiment['compound']
+    if compound_score > 0:
+        positive_count += 1
         return  f'{TextStyle.OKGREEN}Positive{TextStyle.ENDC}'  # Green for positive
 
     elif compound_score < 0:
+        negative_count += 1
+
         return f'{TextStyle.OKRED}Negative{TextStyle.ENDC}'
     else:
+        neutral_count += 1
+
         return f'{TextStyle.OKBLUE}Neutral{TextStyle.ENDC}'
     
 def retrieve_comments(video_id, api_key, page_token=None , max_comments =10):
@@ -72,14 +95,22 @@ def print_comments(comments):
                 print(f'Comment: {cleaned_comment}')
                 print(f'Sentiment: {sentiment}')
                 print('-------------------------')
-                
-    
+def print_all_comments(comments):
+    for comment in comments:
+        snippet = comment['snippet']['topLevelComment']['snippet']
+        text = snippet['textDisplay']
+        cleaned_comment = clean_text(text)
+        analyze_sentiment_vader_all(cleaned_comment)
 # Input YouTube video URL
 video_url = input('Input YouTube URL: ')
 video_id = get_video_id(video_url)
 
 if video_id:
+    
     try:
+        positive_count =0
+        negative_count = 0 
+        neutral_count =0
         # Retrieve video comments
         video_comments_url = f'https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId={video_id}&key={api_key}'
         video_comments_response = requests.get(video_comments_url)
@@ -112,7 +143,7 @@ if video_id:
                 # Retrieve channel details
                 channel_details_url = f'https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id={channel_id}&key={api_key}'
                 channel_details_response = requests.get(channel_details_url)
-
+                 
                 if channel_details_response.status_code == 200:
                     channel_details = channel_details_response.json()['items'][0]['snippet']
                     subscriber_count = channel_details_response.json()['items'][0]['statistics'].get('subscriberCount', 0)
@@ -150,7 +181,13 @@ if video_id:
                 print_comments(more_comments)
             else:
                 break
-            
+        
+        print_all_comments(comments)
+
+# Print totals after processing all comments
+        print(f'Total Positive Comments: {positive_count}')
+        print(f'Total Negative Comments: {negative_count}')
+        print(f'Total Neutral Comments: {neutral_count}')
 
     except Exception as e:
         print(f'An error occurred: {e}')
